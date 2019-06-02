2714b443bc456138cb8f3fadacf7c9c901628a35
Monique Vitche <mvitche7@gmail.com>
2019-06-02 15:01:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
     "Fizz"
-  if int % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
-  if int % 3 && 5 == 0
-    "Fizzbuzz"
-  if int !% 3 || 5 == 0
-    "nil"
   end
 end
